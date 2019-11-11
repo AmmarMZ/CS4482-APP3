0322af8c7a055f2ab16e7708b55dcf5d0193d500
Ammar <ammarmz@hotmail.com>
2019-11-10 23:16:37 -0500

    Added player and player animations

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,33 +6,43 @@ public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private int maxTerrainCount;
     [SerializeField] private List<TerrainData> terrainDatas = new List<TerrainData>();
+    [SerializeField] private Transform terrainHolder;
     private List<GameObject> currentTerrains = new List<GameObject>();
     private Vector3 currentPosition = new Vector3(0, 0, 0);
 
 
     private void Start () {
+        currentPosition = new Vector3(0, 0, 0);
         for (int i = 0; i < maxTerrainCount; i++){
-            SpawnTerrain();
-        }       
+            SpawnTerrain(true);
+        }    
+        maxTerrainCount = currentTerrains.Count;   
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-          SpawnTerrain();
+          SpawnTerrain(false);
         }
     }
     
-    private void SpawnTerrain() {
+    private void SpawnTerrain(bool isStart) {
+
         int whichTerrain = Random.Range(0, terrainDatas.Count);
         int terrainInSuccession = Random.Range(1, terrainDatas[whichTerrain].maxInSuccession);
         
         for (int i = 0; i < terrainInSuccession; i++) {
-            GameObject terrain = Instantiate(terrainDatas[whichTerrain].terrain, currentPosition, Quaternion.identity);
+            GameObject terrain = Instantiate(terrainDatas[whichTerrain].terrain, currentPosition, Quaternion.identity, terrainHolder);
             currentTerrains.Add(terrain);
-            if (currentTerrains.Count > maxTerrainCount) {
-                Destroy(currentTerrains[0]);
-                currentTerrains.RemoveAt(0);
+            if (!isStart) {
+                 if (currentTerrains.Count > maxTerrainCount) {
+                    Destroy(currentTerrains[0]);
+                    currentTerrains.RemoveAt(0);
+                }
             }
+            else {
+
+            }
+           
             currentPosition.x++;
         }
     }
